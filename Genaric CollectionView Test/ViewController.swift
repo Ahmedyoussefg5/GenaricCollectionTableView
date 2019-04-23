ca29f5b9b3f3cda3088c7d0c48bd24ed53b8e0a5
Ahmed Youssef <ahmed.youssef@itgeeks.info>
2019-04-23 16:36:44 +0200

    Initial Commit

---
+++ Genaric CollectionView Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Genaric CollectionView Test
+//
+//  Created by Youssef on 4/23/19.
+//  Copyright © 2019 Youssef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
